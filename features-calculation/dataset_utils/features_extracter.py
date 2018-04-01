9c087f5df1e06fca4fbb81a776543b119a49ca26
Ivan Veselov <veselov.iva@gmail.com>
2018-04-01 11:47:10 +0300

    Add features extracter

---
+++ features-calculation/dataset_utils/features_extracter.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+import numpy
+
+from alphabet import java_alphabet
+from commons import get_text_file_content
+from network import CharacterNetwork
+
+
+def main(network_name, folder):
+    network = CharacterNetwork(network_name, java_alphabet)
+
+    sources = []
+    for root, _, files in os.walk(folder):
+        for file in files:
+            _, ext = os.path.splitext(file)
+            if ext == '.java':
+                sources += [os.path.join(root, file)]
+
+    amount = len(sources)
+    for i, file in enumerate(sources):
+        feature = network.calculate_feature(get_text_file_content(file))
+        numpy.save(file, feature)
+
+        print('%s / %s files are processed' % (i + 1, amount))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2])
