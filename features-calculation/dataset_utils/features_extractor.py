acc1b0a2622df625e8c88b9d93bb87ebb10e6857
Ivan Veselov <veselov.iva@gmail.com>
2018-04-08 21:36:39 +0300

    Modify features extractor.

--- features-calculation/dataset_utils/features_extractor.py
+++ features-calculation/dataset_utils/features_extractor.py
@@ -9,6 +9,11 @@ from network import CharacterNetwork
 
 
 def main(network_name, folder):
+    """
+        Scans <<folder>> for .java files inside it. For each found file feature calculation process
+        is run with network <<network_name>>. Calculated feature is stored beside original file.
+    """
+
     network = CharacterNetwork(network_name, java_alphabet)
 
     sources = []
